4f6bbccbe6129eb19496a974f8d2e223e17f9611
Felipe <felipe13092005@gmail.com>
2024-05-26 12:38:07 -0300

    Melhorando os estilos e adicionando a funcionalidade de deletar o último caracter

--- Desenvolvimeto_Web/calculadora/index.js
+++ Desenvolvimeto_Web/calculadora/index.js
@@ -1,13 +1,16 @@
 sequencia1 = ""
 virgula1 = false
-virgula2 = false
 sequencia2 = ""
+virgula2 = false
 operador = ""
 
 
 function calcular(){
     var entrada = document.getElementsByClassName("caixa")[0]
 
+    //Vai pegar o valor  dos duas sequências e realizar o cálculo
+
+    //Estes blocos condicionais servem para conferir qual cálculo deve-se fazer com base no operador
     if (sequencia2 != ""){
         if(operador == "+"){
             sequencia1 = Number.parseFloat(sequencia1) + Number.parseFloat(sequencia2)
@@ -40,6 +43,14 @@ function calcular(){
             }
         }
         
+
+        //Verifica se a sequência 1 tem ou não vírgula
+        if (parseInt(sequencia1) != parseFloat(sequencia1)){
+            virgula1 = true
+
+        }
+
+        
         virgula2 = false
         entrada.innerText = sequencia1 + operador + sequencia2
     }
@@ -48,6 +59,7 @@ function calcular(){
 
 function concatenar(valor){
     
+    //Ele recebe o valor do botão e adiciona a sequência desejada
     var entrada = document.getElementsByClassName("caixa")[0]
 
 
@@ -81,9 +93,13 @@ function concatenar(valor){
 
 
 function operation(valor){
-   
+
+    if (sequencia1 != ""){
+        operador = valor
+    }
+    //Adiciona um operador ao cálculo
     var entrada = document.getElementsByClassName("caixa")[0]
-    operador = valor
+   
 
     entrada.innerText = sequencia1 + operador + sequencia2
 
@@ -92,6 +108,7 @@ function operation(valor){
 }
 
 function virgula(){
+    //Lógica para colocar vírgula
     var entrada = document.getElementsByClassName("caixa")[0]
    if (sequencia1 == ""){
      sequencia1 = "0."
@@ -123,7 +140,7 @@ function virgula(){
 
 
 function deletar(){
-
+    //Deleta tudo
     var entrada = document.getElementsByClassName("caixa")[0]
     operador = ""
     sequencia1 = ""
@@ -134,5 +151,37 @@ function deletar(){
 }
 
 function subtrair(){
-    //Adicionar funcionalidade de remover o último número 
+    //Deleta o último número
+    var entrada = document.getElementsByClassName("caixa")[0]
+    if (sequencia1 != ""){
+
+        if (operador == ""){
+
+            if (sequencia1.slice(-1) == "."){
+                virgula1 = false
+            }
+
+            sequencia1 = sequencia1.slice(0, -1)
+        }
+
+        else {
+            
+            if (sequencia2 == ""){
+                operador == ""
+            }
+
+
+            else {
+                if (sequencia2.slice(-1) == "."){
+                    virgula2 = false
+                }
+                sequencia2 = sequencia2.slice(0, -1)
+            }
+           
+        }
+
+        entrada.innerText = sequencia1 + operador + sequencia2
+
+    }
+
 }
